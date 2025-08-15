f5388118651b4591e366f1a0c432d909d45e40c8
hllstr <adhytiaahmad2@gmail.com>
2025-08-15 17:26:48 +0700

    feat: add general.go that contains 'ping' commands for checking response & 'info' commands for checking system information

---
+++ commands/general.go
@@ -0,0 +1,131 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waE2E"
+	"go.mau.fi/whatsmeow/types"
+	"google.golang.org/protobuf/proto"
+	"runtime"
+	"sora/lib"
+	"strings"
+	"time"
+)
+
+func init() {
+	Plugin(Cmd{
+		Name:  "ping",
+		Alias: []string{"p"},
+		Desc:  "Check bot response.",
+		// inline code
+		Exec: func(ctx *CommandContext) {
+			now := time.Now()
+			latency := now.Sub(ctx.Message.Info.Timestamp)
+			ctx.Reply(fmt.Sprintf("*Pong!* `%s`", latency.Round(time.Millisecond)))
+		},
+	})
+
+	Plugin(Cmd{
+		Name:  "info",
+		Alias: []string{"i"},
+		Desc:  "Show Information.",
+		Exec:  info,
+	})
+}
+
+// function terpisah
+func getMemoryUsage() string {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	return fmt.Sprintf("%.2f MB", float64(m.Alloc)/1024/1024)
+}
+
+func countCommands() int {
+	count := 0
+	printed := make(map[string]bool)
+	for _, cmd := range Commands {
+		if _, omkeh := printed[cmd.Name]; !omkeh {
+			count++
+			printed[cmd.Name] = true
+		}
+	}
+	return count
+}
+
+func capitalize(s string) string {
+	if s == "" {
+		return s
+	}
+	return strings.ToUpper(s[:1]) + s[1:]
+}
+
+func info(ctx *CommandContext) {
+	bullet := "✦"
+	osInfo, _ := host.Info()
+	cpuInfo, _ := cpu.Info()
+	vmInfo, _ := mem.VirtualMemory()
+
+	platform := fmt.Sprintf("%s/%s", capitalize(runtime.GOOS), runtime.GOARCH)
+	memUsage := getMemoryUsage()
+
+	registeredCmds := countCommands()
+	cpuUseggs, _ := cpu.Percent(time.Second, false)
+	totalCores, err := cpu.Counts(true)
+	if err != nil {
+		return
+	}
+	var sb strings.Builder
+	sb.WriteString("*Sora (空) adalah sebuah Base Bot Wangsaf yang dibangun menggunakan Go (Golang) dan library Whatsmeow.*\n")
+	sb.WriteString(fmt.Sprintf("%s *OS:* %s\n", bullet, capitalize(osInfo.Platform)))
+	sb.WriteString(fmt.Sprintf("%s *CPU:* %s\n", bullet, cpuInfo[0].ModelName))
+	sb.WriteString(fmt.Sprintf("%s *Cores:* %d\n", bullet, totalCores))
+	sb.WriteString(fmt.Sprintf("%s *Freq:* %.2f Mhz\n", bullet, cpuInfo[0].Mhz))
+	sb.WriteString(fmt.Sprintf("%s *RAM:* %.2f GB / %.2f GB\n", bullet, float64(vmInfo.Used)/1024/1024/1024, float64(vmInfo.Total)/1024/1024/1024))
+	sb.WriteString(fmt.Sprintf("%s *Platform:* %s\n", bullet, platform))
+
+	sb.WriteString(fmt.Sprintf("%s *Memory Usage:* %s (Sora Alloc)\n", bullet, memUsage))
+	sb.WriteString(fmt.Sprintf("%s *CPU Usage:* %.2f%%\n", bullet, cpuUseggs[0]))
+	sb.WriteString(fmt.Sprintf("%s *Commands:* %d Registered\n", bullet, registeredCmds))
+	sb.WriteString("> *Download/Clone script nya hanya di https://githuh.com/hllstr/sora*\n> *Jangan lupa kasih star hehe :v*")
+	thumb := "https://i.pinimg.com/originals/7e/2b/fb/7e2bfb2629b8e72826b818a5e749839b.jpg"
+	finalText := sb.String()
+	ctxInfo := &waE2E.ContextInfo{
+		Participant: proto.String(ctx.Message.Info.Sender.String()),
+		ExternalAdReply: &waE2E.ContextInfo_ExternalAdReplyInfo{
+			Title:                 proto.String("✨ System Information ✨"),
+			Body:                  proto.String("A lightweight & high-performance base bot."),
+			ThumbnailURL:          proto.String(thumb),
+			RenderLargerThumbnail: proto.Bool(true),
+			MediaURL:              proto.String(thumb),
+			SourceURL:             proto.String("https://github.com/hllstr/sora"),
+			WtwaAdFormat:          proto.Bool(true),
+			MediaType:             waE2E.ContextInfo_ExternalAdReplyInfo_IMAGE.Enum(),
+		},
+	}
+	if exp, omkeh := lib.GetEphemeralDuration(ctx.Message); omkeh {
+		ctxInfo.Expiration = proto.Uint32(exp)
+	}
+	extra := whatsmeow.SendRequestExtra{}
+	if ctx.Message.Info.Chat.Server == types.GroupServer {
+		ownJID := ctx.Client.Store.ID
+		extra.TargetJID = []types.JID{*ownJID}
+	}
+
+	_, err = ctx.Client.SendMessage(context.Background(), ctx.Message.Info.Chat, &waE2E.Message{
+		ExtendedTextMessage: &waE2E.ExtendedTextMessage{
+			Text:        proto.String(finalText),
+			ContextInfo: ctxInfo,
+		},
+	}, extra)
+	if err != nil {
+		return
+	}
+}
+
+// TODO : nyieun menu.
+// Bypass juga kek nya mau gw buat function aja di lib
+// biar gak import package whatsmeow ama types mulu
--- go.mod
+++ go.mod
@@ -6,18 +6,26 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.28
 	github.com/mdp/qrterminal/v3 v3.2.1
+	github.com/shirou/gopsutil/v3 v3.24.5
 	go.mau.fi/whatsmeow v0.0.0-20250807072145-72ce90b82194
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/petermattis/goid v0.0.0-20250508124226-395b08cebbdb // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.mau.fi/libsignal v0.2.0 // indirect
 	go.mau.fi/util v0.8.8 // indirect
 	golang.org/x/crypto v0.40.0 // indirect
