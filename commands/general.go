5c2ecfe45ee2f17c608af926817054ac79a2768f
hllstr <adhytiaahmad2@gmail.com>
2025-08-17 11:43:50 +0700

    refactor: use bypass helper function & change description language to english

--- commands/general.go
+++ commands/general.go
@@ -6,9 +6,7 @@ import (
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/mem"
-	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/proto/waE2E"
-	"go.mau.fi/whatsmeow/types"
 	"google.golang.org/protobuf/proto"
 	"runtime"
 	"sora/lib"
@@ -79,19 +77,20 @@ func info(ctx *CommandContext) {
 		return
 	}
 	var sb strings.Builder
-	sb.WriteString("*Sora (空) adalah sebuah Base Bot Wangsaf yang dibangun menggunakan Go (Golang) dan library Whatsmeow.*\n")
+	sb.WriteString("*Sora (空) is a Simple Base Bot WhatsApp written in Go using Whatsmeow Library.*\n")
 	sb.WriteString(fmt.Sprintf("%s *OS:* %s\n", bullet, capitalize(osInfo.Platform)))
 	sb.WriteString(fmt.Sprintf("%s *CPU:* %s\n", bullet, cpuInfo[0].ModelName))
 	sb.WriteString(fmt.Sprintf("%s *Cores:* %d\n", bullet, totalCores))
 	sb.WriteString(fmt.Sprintf("%s *Freq:* %.2f Mhz\n", bullet, cpuInfo[0].Mhz))
 	sb.WriteString(fmt.Sprintf("%s *RAM:* %.2f GB / %.2f GB\n", bullet, float64(vmInfo.Used)/1024/1024/1024, float64(vmInfo.Total)/1024/1024/1024))
 	sb.WriteString(fmt.Sprintf("%s *Platform:* %s\n", bullet, platform))
-
 	sb.WriteString(fmt.Sprintf("%s *Memory Usage:* %s (Sora Alloc)\n", bullet, memUsage))
-	// CPU usex bikin delay 1 sex jir buat ngukurnya, gw comment aja, kalo lu mau nampilin cpu usage bisa di uncomment sajah.
+	// CPU usex bikin delay 1 sex jir buat ngukurnya,makanya gw comment aja, kalo lu mau nampilin cpu usage bisa di uncomment sajah.
 	// sb.WriteString(fmt.Sprintf("%s *CPU Usage:* %.2f%%\n", bullet, cpuUseggs[0]))
+	// or nanti gw buat kek Edit aja, pesan awal nanti bkalan measuring... terus 1 detik kemudian muncul usage nya
+	// tapi nanti aja deng, mager..
 	sb.WriteString(fmt.Sprintf("%s *Commands:* %d Registered\n", bullet, registeredCmds))
-	sb.WriteString(fmt.Sprintf("> *Download/Clone script nya hanya di %s*\n> *Jangan lupa kasih star hehe :v*", myrepo))
+	sb.WriteString(fmt.Sprintf("> *Source Code : %s*\n> *Don't forget to give a star ✨٩(ˊᗜˋ*)و✨*", myrepo))
 	thumb := "https://i.pinimg.com/originals/7e/2b/fb/7e2bfb2629b8e72826b818a5e749839b.jpg"
 	finalText := sb.String()
 	ctxInfo := &waE2E.ContextInfo{
@@ -101,7 +100,6 @@ func info(ctx *CommandContext) {
 			Body:                  proto.String("A lightweight & high-performance base bot."),
 			ThumbnailURL:          proto.String(thumb),
 			RenderLargerThumbnail: proto.Bool(true),
-			MediaURL:              proto.String(thumb),
 			SourceURL:             proto.String(myrepo),
 			WtwaAdFormat:          proto.Bool(true),
 			MediaType:             waE2E.ContextInfo_ExternalAdReplyInfo_IMAGE.Enum(),
@@ -110,24 +108,18 @@ func info(ctx *CommandContext) {
 	if exp, omkeh := lib.GetEphemeralDuration(ctx.Message); omkeh {
 		ctxInfo.Expiration = proto.Uint32(exp)
 	}
-	extra := whatsmeow.SendRequestExtra{}
-	if ctx.Message.Info.Chat.Server == types.GroupServer {
-		ownJID := ctx.Client.Store.ID
-		extra.TargetJID = []types.JID{*ownJID}
-	}
-
+	// bypass participant sekarang lebih gampang dipake, udah gw jadiin function di lib
+	bypass := lib.Bypass(ctx.Client, ctx.Message.Info.Chat)
 	_, err = ctx.Client.SendMessage(context.Background(), ctx.Message.Info.Chat, &waE2E.Message{
 		ExtendedTextMessage: &waE2E.ExtendedTextMessage{
 			Text:        proto.String(finalText),
 			ContextInfo: ctxInfo,
 			MatchedText: proto.String(myrepo),
 		},
-	}, extra)
+	}, bypass)
 	if err != nil {
 		return
 	}
 }
 
 // TODO : nyieun menu.
-// Bypass juga kek nya mau gw buat function aja di lib
-// biar gak import package whatsmeow ama types mulu
